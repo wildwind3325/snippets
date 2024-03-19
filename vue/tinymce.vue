d62656298f953f95173b63e5f8358b19f6536a6f
zhangjun <zhangjun@onwings.com.cn>
2024-03-19 11:02:08 +0800

    tinymce

---
+++ vue/tinymce.vue
@@ -0,0 +1,22 @@
+<template>
+  <Editor v-model="content" :init="options" />
+</template>
+
+<script>
+import Editor from '@tinymce/tinymce-vue';
+export default {
+  name: 'Home',
+  components: { Editor },
+  data() {
+    return {
+      options: {
+        language: 'zh-Hans',
+        branding: false,
+        menubar: false,
+        resize: false
+      },
+      content: ''
+    };
+  }
+};
+</script>
